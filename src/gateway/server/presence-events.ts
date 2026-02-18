c201f3d89a2d39b85cdfe27d4230b20909beefe4
Gustavo Madeira Santana <gumadeiras@gmail.com>
2026-02-17 20:34:48 -0500

    style: format gateway presence routing files

--- src/gateway/server/presence-events.ts
+++ src/gateway/server/presence-events.ts
@@ -1,5 +1,5 @@
-import type { GatewayBroadcastFn } from "../server-broadcast.js";
 import { listSystemPresence } from "../../infra/system-presence.js";
+import type { GatewayBroadcastFn } from "../server-broadcast.js";
 
 export function broadcastPresenceSnapshot(params: {
   broadcast: GatewayBroadcastFn;
